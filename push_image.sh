568d57bf1e15e84b20c743633d201a695e4d9dc5
Harish Agrawal <harish@zb.io>
2020-05-07 22:52:07 +0530

    ZBIO-120: Changes to build googlebookapi and deploy to ZKE

--- push_image.sh
+++ push_image.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
- ROOST_DIR=/var/tmp/Roost
- ZKE_NODE_DIR=$ROOST_DIR/nodes
- SSHOPTS="-i $ROOST_DIR/.ssh/id_rsa -o ConnectTimeout=5 -o LogLevel=ERROR -o StrictHostKeyChecking=false -o UserKnownHostsFile=/dev/null"
+ . ~/.roost_user_env
+ SSHOPTS="-i $USER_HOME/.ssh/roost_id_rsa -o ConnectTimeout=5 -o LogLevel=ERROR -o StrictHostKeyChecking=false -o UserKnownHostsFile=/dev/null"
  IMAGE=$1
  IMAGE_TAR="$$.tar"
 
- if [ "$HOST" != "roost-utility" ]; then
+ if [ "$HOSTNAME" != "roost-utility" ]; then
      echo "Skip loading image to ZKE"
  else
  docker save $IMAGE > $IMAGE_TAR
- /usr/bin/find $ZKE_NODE_DIR -mindepth 1 -maxdepth 1 -type d -not -name tmp | while read node;
+ kubectl get nodes -o name | sed -e 's#node/##g' | grep -v master | while read node;
  do
-    myprint "Copying $IMAGE_TAR  to $node"
     echo "scp $SSHOPTS $IMAGE_TAR $USER@$node:/tmp"
     scp $SSHOPTS $IMAGE_TAR $USER@$node:/tmp
     cmd_to_execute="docker load < /tmp/$IMAGE_TAR"
+    echo "ssh $SSHOPTS $USER@$node $cmd_to_execute"
     ssh $SSHOPTS $USER@$node $cmd_to_execute
   done
  fi
