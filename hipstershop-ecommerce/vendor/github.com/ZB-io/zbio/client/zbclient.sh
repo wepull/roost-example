6740a209fb2c71ad82b74dba9e6651e098e7dafc
Harish Agrawal <h.asterix@gmail.com>
2020-07-07 20:16:21 +0530

    ZBIO-648: Remove the unused samples
    extra folders removed to reduce the zip size

--- hipstershop-ecommerce/vendor/github.com/ZB-io/zbio/client/zbclient.sh
+++
@@ -1,8 +0,0 @@
-#"go","test","-v","-count=1","-timeout=30s","/go/src/github.com/ZB-io/zbio/client","-run=TestCreateTopic"
-#go test -v /go/src/github.com/ZB-io/zbio/client
-#go test -v -count=1 -timeout=30s /go/src/github.com/ZB-io/zbio/client -run=TestCreateTopic
-echo "Calling ZBClient"
-export SERVER_CRT=/zbclient/security/cert/server.crt && ./client.test -test.run CreateDuplicateTopics
-#export SERVER_CRT=/zbclient/security/cert/server.crt && ./client.test -test.run CreateSingleTopic -test.count 1000
-export SERVER_CRT=/zbclient/security/cert/server.crt && ./client.test -test.run NewMessage -test.count 1000
-
--- pub-sub/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/ZB-io/zbio-example/pub-sub
-
-go 1.13
-
-require (
-	github.com/ZB-io/zbio v0.0.6
-	google.golang.org/protobuf v1.22.0 // indirect
-)
