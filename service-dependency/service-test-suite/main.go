8e65665e95ed52a9643226591ce982bde43a0d3f
Mayank G <8110509+mgdevstack@users.noreply.github.com>
2021-02-25 21:27:07 +0530

    update service test suite

--- service-dependency/service-test-suite/main.go
+++ service-dependency/service-test-suite/main.go
@@ -3,17 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
+var res string = "PASS"
+
 func runTest(w http.ResponseWriter, r *http.Request) {
+	i := rand.Intn(10)
+	log.Printf("Endpoint Hit: runTest %d\n", i)
+	if i < 5 {
+		res = "FAILED"
+	} else {
+		res = "IN-PROCESS"
+		log.Printf("sleeping for %d seconds\n", i)
+		time.Sleep(time.Second * time.Duration(i))
+		res = "PASS"
+	}
 	fmt.Fprintf(w, "ok")
-	fmt.Println("Endpoint Hit: runTest")
 }
 
 func testResult(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "PASS")
-	fmt.Println("Endpoint Hit: testResult")
+	fmt.Fprintf(w, res)
+	log.Println("Endpoint Hit: testResult:", res)
 }
 
 func handleRequests() {
@@ -23,6 +36,6 @@ func handleRequests() {
 }
 
 func main() {
-	log.Printf("service test suite is started...")
+	log.Printf("service test suite is started...\n")
 	handleRequests()
 }
